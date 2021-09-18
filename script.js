081aaa83cfa9f369138f6d8c84ab7646c0aea248
kilokid <arlomov007@gmail.com>
2021-09-18 13:27:05 +0300

    fix2: homework

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const num = 266219;
 
 const numArr = Array.from(String(num), Number);
-let res = 1;
-
-for (let i = 0; i <= numArr.length - 1; i++) {
-    res *= numArr[i];
-    console.log(res);
-}
+let res = numArr.reduce((sum, current) => sum * current);
+console.log(res);
 
 res = res ** 3;
 
