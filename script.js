ab43c4401a7c601a8abb1bf3324cf7b7b81df765
kilokid <arlomov007@gmail.com>
2021-09-21 21:39:55 +0300

    finish hard homework 04

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-const lang = 'ru';
-const ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-// Решите задачу через if
-if (lang === 'ru') {
-    console.log(ruDay);
-} else {
-    console.log(enDay);
+function examination(str) {
+    if (str != String(str)) {
+        console.log('Была передана не строка');
+    } else if (str.length > 30) {
+        str = str.slice(0, 30) + '...';
+    }
+    console.log(str.trim());
 }
-
-// Решите задачу через switch-case
-switch (lang) {
-    case 'ru':
-        console.log(ruDay);
-        break;
-    case 'en':
-        console.log(enDay);
-        break;
-    default:
-        console.log('Непредвиденное значение!');
-}
-
-// Решите задачу через многомерный массив без ифов и switch
-const days = new Map();
-days.set('ru', ruDay);
-days.set('en', enDay);
-console.log(days.get(lang));
-
-/*
-У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести 
-в консоль “директор”, если значение “Максим” то вывести в консоль “преподаватель”, 
-с любым другим значением вывести в консоль “студент”
-Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-*/
-const namePerson = 'Артем';
-namePerson === 'Артем' ? console.log('Директор') : 
-(namePerson === 'Максим') ? console.log('Преподаватель') 
-: console.log('Студент');
+examination('Hello world!');
