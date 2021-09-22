f27b137d5336308e57eb91271ba03d71fd10baa6
kilokid <arlomov007@gmail.com>
2021-09-22 20:13:13 +0300

    fix: 1

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const arr = [];
-arr.push('32', '25', '2132', '88', '1323', '4123', '768');
-arr.forEach((i) => {
-    if (i[0] == 2 || i[0] == 4) {
-        console.log(i);
+const arr = ['232', '5', '132', '88', '1323', '123', '768'];
+let res = [];
+arr.forEach((item) => {
+    if (item[0] == 2 || item[0] == 4) {
+        res.push(item);
     }
 });
+console.log(res);
 
-nextPrime:
-for (let i = 2; i <= 100; i % i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j == 0) continue nextPrime;
-    }
-    console.log(`${i} Делители этого числа: 1 и ${i}`)
-}
+
+// nextPrime:
+// for (let i = 2; i <= 100; i % i++) {
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) continue nextPrime;
+//     }
+//     console.log(`${i} Делители этого числа: 1 и ${i}`)
+// }
