6ddad56f77d28547e4b68d7ddbf04fe5206f9ef9
kilokid <arlomov007@gmail.com>
2021-09-22 17:51:36 +0300

    fix if else && new map

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const lang = 'ru';
-const ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const ruWeekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const enWeekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 // Решите задачу через if
 if (lang === 'ru') {
-    console.log(ruDay);
+    console.log(ruWeekDays);
+} else if (lang === 'en') {
+    console.log(enWeekDays);
 } else {
-    console.log(enDay);
+    console.log('Непредвиденное значение!');
 }
 
 // Решите задачу через switch-case
 switch (lang) {
     case 'ru':
-        console.log(ruDay);
+        console.log(ruWeekDays);
         break;
     case 'en':
-        console.log(enDay);
+        console.log(enWeekDays);
         break;
     default:
         console.log('Непредвиденное значение!');
 }
 
 // Решите задачу через многомерный массив без ифов и switch
-const days = new Map();
-days.set('ru', ruDay);
-days.set('en', enDay);
+const daysOfTheWeek = new Map([
+    ['ru', ruWeekDays],
+    ['en', enWeekDays]
+]);
 console.log(days.get(lang));
 
 /*
