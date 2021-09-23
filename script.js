5f832162b8c9d558d12e7c4e512370a2ded431ed
kilokid <arlomov007@gmail.com>
2021-09-23 22:00:40 +0300

    fix homework

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const arr = ['232', '5', '132', '88', '1323', '123', '768'];
-let res = [];
-arr.forEach((item) => {
-    if (item[0] == 2 || item[0] == 4) {
-        res.push(item);
-    }
-});
-console.log(res);
+// const arr = ['232', '5', '132', '88', '1323', '123', '768'];
+// let res = [];
+// arr.forEach((item) => {
+//     if (item[0] == 2 || item[0] == 4) {
+//         res.push(item);
+//     }
+// });
+// console.log(res);
 
 
 // nextPrime:
@@ -15,3 +15,22 @@ console.log(res);
 //     }
 //     console.log(`${i} Делители этого числа: 1 и ${i}`)
 // }
+
+function isPrime(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+        return false;
+    }
+    return num > 1;
+  }
+}
+
+const array = [];
+for (let i = 0; i < 100; i++) {
+    array.push(i);
+}
+
+array.forEach((i) => {
+    isPrime(i);
+    console.log(i);
+});
