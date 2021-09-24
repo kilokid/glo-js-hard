f387a10aa5c53c979f543900673bea6aed5abb9c
kilokid <arlomov007@gmail.com>
2021-09-25 01:38:04 +0300

    fix hard homework 05

--- script.js
+++ script.js
@@ -7,30 +7,19 @@
 // });
 // console.log(res);
 
-
-// nextPrime:
-// for (let i = 2; i <= 100; i % i++) {
-//     for (let j = 2; j < i; j++) {
-//         if (i % j == 0) continue nextPrime;
-//     }
-//     console.log(`${i} Делители этого числа: 1 и ${i}`)
-// }
-
-function isPrime(num) {
-  for(let i = 2; i < num; i++) {
-    if(num % i === 0) {
-        return false;
+function simpleNum(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
     }
     return num > 1;
-  }
 }
 
-const array = [];
-for (let i = 0; i < 100; i++) {
-    array.push(i);
+let arr = [];
+for (let i = 2; i < 100; i++) {
+    if (simpleNum(i)) {
+        arr.push(i);
+    }
 }
-
-array.forEach((i) => {
-    isPrime(i);
-    console.log(i);
-});
+console.log(arr);
