f9243a0a88b7a4afc686adc6891465280ce38f78
kilokid <arlomov007@gmail.com>
2021-09-25 18:10:56 +0300

    super finish fix!

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const arr = ['232', '2', '432', '88', '4323', '123', '768'];
 
-const res = arr.filter(item => item[0] === '2' || item[0] === '4').forEach(item => console.log(item));
+const res = arr
+    .filter(item => item[0] === '2' || item[0] === '4')
+    .forEach(item => console.log(item));
 
 
-function primeNum(num) {
+function isprimeNum(num) {
     for (let i = 2; i < num; i++) {
         if (num % i === 0) {
             return false;
@@ -12,8 +14,10 @@ function primeNum(num) {
     return num > 1;
 }
 
-const array = [];
-for (let i = 1; i < 100; i++) {
-    array.push(i);
+const numbers = [];
+for (let i = 1; i <= 100; i++) {
+    numbers.push(i);
 }
-array.filter(item => primeNum(item)).forEach(item => console.log(item));
+numbers
+    .filter(item => isprimeNum(item))
+    .forEach(item => console.log(item + ` Делители этого числа: 1 и ${item}`));
