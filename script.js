d83fc5cdfdfccfb82070b283d529fbf2edd96fb7
kilokid <arlomov007@gmail.com>
2021-09-28 02:51:19 +0300

    finish homework

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-const arr = ['232', '2', '432', '88', '4323', '123', '768'];
+'use strict';
 
-const res = arr
-    .filter(item => item[0] === '2' || item[0] === '4')
-    .forEach(item => console.log(item));
+const week  = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const day = document.querySelectorAll('.day-of-week');
+const today = new Date();
 
-
-function isprimeNum(num) {
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false;
-        }
+week.forEach((item, i, week) => {
+    if (item === 'Суббота' || item === 'Воскресенье') {
+        day[i].innerHTML = week[i].italics();
+    } else if (i === today.getDay() - 1) {
+        day[i].innerHTML = week[i].bold();
+    } else {
+        day[i].innerHTML = week[i];
     }
-    return num > 1;
-}
-
-const numbers = [];
-for (let i = 1; i <= 100; i++) {
-    numbers.push(i);
-}
-numbers
-    .filter(item => isprimeNum(item))
-    .forEach(item => console.log(item + ` Делители этого числа: 1 и ${item}`));
+});
