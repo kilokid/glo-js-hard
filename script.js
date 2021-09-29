0c332f443c092e35320d7bcbbd0660b015db9b46
kilokid <arlomov007@gmail.com>
2021-09-30 02:57:30 +0300

    fix: starting from scratch

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 'use strict';
 
-// const week  = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-// const today = new Date();
-// let numWeekDay = today.getDay();
-
-// if (numWeekDay === 0) {
-//     numWeekDay = 6;
-// } else {
-//     numWeekDay--;
-// }
-
-// week.forEach((day, i) => {
-//     let str = day;
-//     if (i === numWeekDay) {
-//         str = `<b>${day}</b>`;
-//     } else {
-//         str = `${day}`;
-//     }
-
-//     if (i === 5 || i === 6) {
-//         str = `<i>${str}</i>`;
-//     }
-//     document.body.nsertAdjacentHTML('beforeend', `<div>${str}</div>`);
-// });
-
 const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const div = document.createElement('div');
 document.body.append(div);
@@ -35,9 +11,8 @@ week.forEach((day, i) => {
     let str = day;
     if (i === (numWeekDay - 1)) {
         str = `<b>${day}</b>`;
-    } else {
-        str = `${day}`;
     }
+
     if (i === 5 || i === 6) {
         str = `<i>${str}</i>`; 
     }
