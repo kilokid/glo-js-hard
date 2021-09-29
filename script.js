fb11ddb0335ff7573406d77e08e1c5bec595cc93
kilokid <arlomov007@gmail.com>
2021-09-30 02:58:32 +0300

    start: hard homework 09

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const div = document.createElement('div');
-document.body.append(div);
-
-const today = new Date();
-let numWeekDay = today.getDay();
-
-week.forEach((day, i) => {
-    let str = day;
-    if (i === (numWeekDay - 1)) {
-        str = `<b>${day}</b>`;
-    }
-
-    if (i === 5 || i === 6) {
-        str = `<i>${str}</i>`; 
-    }
-    div.insertAdjacentHTML('beforeend', `<p>${str}</p>`);
-});
